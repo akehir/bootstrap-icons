0f6a3af75b65ea4d686a04d2be3d04dfe3740ffb
Raphael Ochsenbein <raphael@akehir.com>
2023-07-22 23:12:19 +0200

    Fix more tests

--- projects/example-app-standalone/src/app/app.component.spec.ts
+++ projects/example-app-standalone/src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('example-app-standalone app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('example-app-standalone');
   });
 });

c8a843d4f65fd6b5bc9cfd9c25c8df56d4c39ec0
Raphael Ochsenbein <raphael@akehir.com>
2025-05-30 23:17:51 +0200

    Fix build

--- projects/bootstrap-icons-lib/src/lib/icons.registry.ts
+++ projects/bootstrap-icons-lib/src/lib/icons.registry.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { BootstrapIcon } from '../icons';
+
+interface AnyRegistryIcon {
+  name: string;
+  data: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +12,8 @@ export class BootstrapIconsRegistry {
 
   private registry = new Map<string, string>();
 
-  public registerIcons(icons: BootstrapIcon[]): void {
-    icons.forEach((icon: BootstrapIcon) => this.registry.set(icon.name, icon.data));
+  public registerIcons(icons: AnyRegistryIcon[]): void {
+    icons.forEach((icon: AnyRegistryIcon) => this.registry.set(icon.name, icon.data));
   }
 
   public getIcon(iconName: string): string | undefined {
